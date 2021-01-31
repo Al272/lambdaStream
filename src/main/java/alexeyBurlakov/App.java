f391fdb954bcd0fede5c8a676ba60792aa9c893b
Al272 <54018766+Al272@users.noreply.github.com>
2021-01-31 22:55:45 +0300

    nerFef
    # Conflicts:
    #	src/main/java/App.java
    #	src/main/java/Methods.java
    #	src/main/java/Organization.java
    #	src/main/java/Stock.java

---
+++ src/main/java/alexeyBurlakov/App.java
@@ -0,0 +1,26 @@
+package alexeyBurlakov;
+
+import alexeyBurlakov.entity.Organization;
+import alexeyBurlakov.methods.Methods;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Methods methods = new Methods();
+        ObjectMapper mapper = new ObjectMapper();
+        //alexeyBurlakov.entity.Organization org = new alexeyBurlakov.entity.Organization("Albatros","shortAlb","Moscow","02","522","0202","1985.07.11");
+        //mapper.writeValue(new File("C:\\Users\\Алексей Б\\Desktop\\org.json"), org);
+        Organization [] orgArr = mapper.readValue(new File("C:\\Users\\Алексей Б\\Desktop\\org2.json"), Organization[].class);
+        methods.information(orgArr);
+        methods.closePapers(orgArr);
+        methods.dateOrganization("01.02.1966",orgArr);
+        methods.stockCurrency("eur" ,orgArr);
+    }
+
+
+
+
+}
---
+++ src/main/java/alexeyBurlakov/entity/Organization.java
@@ -0,0 +1,97 @@
+package alexeyBurlakov.entity;
+
+import alexeyBurlakov.entity.Stock;
+
+import java.time.LocalDate;
+
+public class Organization {
+    private String name;
+    private String shortName;
+    private String address;
+    private String phone;
+    private String inn;
+    private String ogrn;
+    private String date;
+    private Stock[] papers;
+
+    public Organization(){
+
+    }
+    public Organization(String name, String shortName, String address, String phone, String inn, String ogrn, String date){
+        this.name = name;
+        this.shortName = shortName;
+        this.address = address;
+        this.phone = phone;
+        this.inn = inn;
+        this.ogrn = ogrn;
+        this.date = date;
+        //this.papers = papers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getInn() {
+        return inn;
+    }
+
+    public void setInn(String inn) {
+        this.inn = inn;
+    }
+
+    public String getOgrn() {
+        return ogrn;
+    }
+
+    public void setOgrn(String ogrn) {
+        this.ogrn = ogrn;
+    }
+
+    public LocalDate getDate() {
+       String [] temp = date.split("\\.");
+       return LocalDate.of(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Integer.parseInt(temp[2]));
+
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Stock[] getPapers() {
+        return papers;
+    }
+
+    public void setPapers(Stock[] papers) {
+        this.papers = papers;
+    }
+
+}
